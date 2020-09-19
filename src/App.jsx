33165cec562e095ac9ae4d8e2aace5ee06d3c598
marcelaCalderon <ccalderongir@gmail.com>
2020-09-19 10:55:03 -0500

    Cambio desde BranchDevelopment. Se refactoriza proyecto y añade componente Numbers

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Button from './components/Button'
 import Functions from './components/Functions'
 import MathOperations from './components/MathOperations'
+import Numbers from './components/Numbers'
 import Result from './components/Result'
 import './App.css'
 
@@ -18,18 +19,11 @@ const App = () => {
     return (
     <main className='react-calculator' >
         <Result />
-        <div className="numbers">
-            <Button text="1" clickHandler={clickHandlerFunction}></Button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-        </div>
+        <Numbers
+            onClickNumber ={number=>
+                console.log("Numbers", number)
+            }
+        />
 
         <Functions
             onContentClear={() => 
